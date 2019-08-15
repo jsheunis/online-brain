c78a2886ea915107d0fcf1595744878636d95e62
drgs <d.m.serban@student.tue.nl>
2019-08-15 19:26:49 +0200

    fcs: Set file copy time from command line

--- file_copy_script.py
+++ file_copy_script.py
@@ -2,11 +2,13 @@ import os
 import shutil
 import threading
 import datetime
+import sys
 
 INITIAL_DIRECTORY = 'sample_data/'
 DESTINATION_DIRECTORY = 'test_data/'
 
-FILE_GENERATION_TIME = 4.0
+# TODO: Add conditions for argument (min/max)
+file_generation_time = float(sys.argv[1]) / 1000
 
 # Get a list of all files present in the source directory
 files = []
@@ -20,7 +22,7 @@ files.sort()
 
 # Copy each file every two seconds
 def copy_files():
-    timer = threading.Timer(FILE_GENERATION_TIME, copy_files)
+    timer = threading.Timer(file_generation_time, copy_files)
     timer.start()
     if len(files) != 0:
         file = files.pop(0)
@@ -32,5 +34,6 @@ def copy_files():
     else:
         timer.cancel()
         print("Job finished - ", datetime.datetime.now())
-        
+
+
 copy_files()
